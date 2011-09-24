74ffa23cf653e1ef320739a494f2fd40ee3e7c57
Danneh <Danneh@touma>
2011-09-24 14:53:03 -0700

    google calc added, if calc returns a result for 'google query, that's returned

--- modules/google.py
+++ modules/google.py
@@ -24,6 +24,12 @@ class google(Module):
         }
     
     def google_search(self, event, command):
+        calc_result = self.google_calc_search(command.arguments)
+        if calc_result:
+            response = '*** /c12G/c4o/c8o/c12g/c3l/c4e/c: ' + calc_result
+            self.bot.irc.servers[event.server].privmsg(event.from_to, response)
+            return
+        
         encoded_query = urllib.parse.urlencode({b'q' : unescape(command.arguments)})
         url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (encoded_query)
         try:
@@ -39,6 +45,27 @@ class google(Module):
         response = '*** /c12G/c4o/c8o/c12g/c3l/c4e/c: ' + url_result
         
         self.bot.irc.servers[event.server].privmsg(event.from_to, response)
+    
+    def google_calc_search(self, query):
+        url = 'http://www.google.com/ig/calculator?'
+        url += urllib.parse.urlencode({b'q' : unescape(query)})
+        
+        calc_result = urllib.request.urlopen(url)
+        calc_read = calc_result.read().decode('utf-8')
+        calc_split = calc_read.split('"')
+        
+        try:
+            #q_from = json_result['lhs']
+            #q_to = json_result['rhs']
+            q_from = calc_split[1]
+            q_to = calc_split[3]
+            if q_from == '' or q_to == '':
+                return False
+            final_result = '/i' + escape(q_from) + '/i is /i' + escape(q_to) + '/i'
+        except:
+            final_result = False
+        
+        return final_result
     
     def youtube_search(self, event, command):
         encoded_query = urllib.parse.urlencode({b'q' : unescape(command.arguments)})
