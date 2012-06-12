2728b2ce771ac71c5c08b3863ca3eb3bf338dda0
Daniel Oakley <danneh@danneh.net>
2012-06-12 20:19:51 +1000

    Now autodownloads/installs colorama

--- goshu.py
+++ goshu.py
@@ -7,6 +7,8 @@
 # ----------------------------------------------------------------------------
 # Goshubot IRC Bot    -    http://danneh.net/goshu
 
+import os
+
 # fix for Windows' shitty default cp1252 encoding unicode output errors
 import sys
 if sys.stdout.encoding.lower() != 'utf-8':
@@ -14,6 +16,42 @@ if sys.stdout.encoding.lower() != 'utf-8':
     warnings.warn('unicode may be mangled due to non utf-8 stdout encoding: '+sys.stdout.encoding, UnicodeWarning)
     import io
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=sys.stdout.encoding, errors='replace', line_buffering=True)
+
+#automatically download/install colorama if needed
+try:
+	import colorama
+except ImportError:
+	download_url = 'http://pypi.python.org/packages/source/c/colorama/colorama-0.2.4.zip#md5=f1cf0b0bc675e0b9e22df28747ea1694'
+	zip_path = 'autoinstall' + os.sep + 'colorama-0.2.4.zip'
+	unzip_path = 'autoinstall' + os.sep + 'colorama-0.2.4'
+
+	print('Colorama not found on system. Attempting to download from:')
+	print(download_url)
+
+	# download
+	import urllib.request, urllib.parse, urllib.error
+
+	if not os.path.exists('autoinstall'):
+		os.makedirs('autoinstall')
+
+	file = open(zip_path, 'wb')
+	file.write(urllib.request.urlopen(download_url).read())
+	file.close()
+
+	# unzip
+	import zipfile
+
+	unzip = zipfile.ZipFile(zip_path)
+	unzip.extractall('autoinstall')
+
+	# and install
+	old_path = os.getcwd()
+	os.chdir(unzip_path)
+	print(os.getcwd())
+	os.system(sys.executable + ' ' + 'setup.py build')
+	os.system(sys.executable + ' ' + 'setup.py install')
+	os.chdir(old_path)
+
 
 import gbot.bot
 
