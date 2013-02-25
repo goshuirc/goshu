a9cda85eb0571f9006446435e91bf60941769677
Daniel Oaks <danneh@danneh.net>
2013-02-25 14:09:26 +1000

    And removing some useless code

--- modules/apiquery/youtube_lnk.py
+++ modules/apiquery/youtube_lnk.py
@@ -2,9 +2,6 @@ from gbot.libs.helper import time_metric, metric
 
 
 def length(format_json, input_json):
-    for i in range(30):
-        print(input_json)
-        print('')
     return time_metric(secs=input_json['data']['items'][0]['duration'])
 
 
