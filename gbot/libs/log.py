ee0fd63de38f8693a31eb4dc8497b36285c05dd2
Daniel Oaks <daniel@danieloaks.net>
2015-08-27 21:28:43 +1000

    log: Remove empty, unused logger

--- gbot/libs/log.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Goshu IRC Bot
-# written by Daniel Oaks <daniel@danieloaks.net>
-# licensed under the ISC license
-
-class Logger:
-	"""Provides a basic logging interface including debug, informational, etc configurable logging."""
-	pass
