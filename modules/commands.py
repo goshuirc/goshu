f415b139dd7642c67d825b7a1d24ecd7890a89e9
Daniel Oaks <daniel@danieloaks.net>
2015-09-08 22:09:39 +1000

    mod-commands: Fix to work with girc

--- modules/commands.py
+++ modules/commands.py
@@ -12,50 +12,61 @@ class commands(Module):
     core = True
 
     # irc
-    def cmd_msg(self, event, command, usercommand):
-        """Send a /msg
-
-        @usage <target> <message>
-        @call_level admin
-        """
-        msg_target, msg_msg = split_num(usercommand.arguments)
-
-        event['server'].privmsg(msg_target, msg_msg)
-
-    def cmd_me(self, event, command, usercommand):
+    def acmd_me(self, event, command, usercommand):
         """Send a /me
 
+        @global
         @usage <target> <message>
-        @call_level admin
         """
         msg_target, msg_msg = split_num(usercommand.arguments)
 
         event['server'].action(msg_target, msg_msg)
 
-    def cmd_join(self, event, command, usercommand):
+    def acmd_msg(self, event, command, usercommand):
+        """Send a /msg
+
+        @global
+        @usage <target> <message>
+        """
+        msg_target, msg_msg = split_num(usercommand.arguments)
+
+        event['server'].msg(msg_target, msg_msg)
+
+    def acmd_notice(self, event, command, usercommand):
+        """Send a /notice
+
+        @global
+        @usage <target> <message>
+        """
+        msg_target, msg_msg = split_num(usercommand.arguments)
+
+        event['server'].notice(msg_target, msg_msg)
+
+    def acmd_join(self, event, command, usercommand):
         """Join a channel
 
+        @global
         @usage <channel>
-        @call_level admin
         """
         channel, key = split_num(usercommand.arguments)
 
-        event['server'].join(channel, key)
+        event['server'].join_channel(channel, key)
 
-    def cmd_part(self, event, command, usercommand):
+    def acmd_part(self, event, command, usercommand):
         """Leave a channel
 
+        @global
         @usage <channel>
-        @call_level admin
         """
         channel, reason = split_num(usercommand.arguments)
 
-        event['server'].part(channel, reason)
+        event['server'].part_channel(channel, reason)
 
     # goshu control
-    def cmd_shutdown(self, event, command, usercommand):
+    def acmd_shutdown(self, event, command, usercommand):
         """Shutdown bot
 
+        @global
         @usage [quit message]
         @call_level superadmin
         """
