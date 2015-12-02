d49d55b9922c476eb42abac16ee74a46eadb938c
Daniel Oaks <daniel@danieloaks.net>
2015-12-02 20:12:28 +1000

    modules.link: Cooldown URL linking

--- modules/link/__init__.py
+++ modules/link/__init__.py
@@ -8,11 +8,33 @@
 # In particular, the regexes and the display layout. Thanks a bunch, bro!
 
 import re
+import time
 
 from girc.formatting import escape, unescape
+from girc.utils import CaseInsensitiveDict
 
 from gbot.libs.helper import get_url, format_extract, JsonHandler
 from gbot.modules import Module
+
+
+class Cooldown:
+    def __init__(self, cooldown_seconds=15, multiple=2, max_cooldown=60*60*24):
+        self.cooldown_seconds = cooldown_seconds
+        self.end_ts = time.time()
+        self.default_multiple = multiple
+        self.multiple = multiple
+        self.max_cooldown = max_cooldown
+
+    def can_do(self):
+        if time.time() > self.end_ts:
+            self.multiple = self.default_multiple
+            self.end_ts = time.time() + self.cooldown_seconds
+            return True
+
+        self.multiple *= self.default_multiple
+        cooldown = min(self.cooldown_seconds * self.multiple, self.max_cooldown)
+        self.end_ts = time.time() + cooldown
+        return False
 
 
 class link(Module):
@@ -21,6 +43,7 @@ class link(Module):
     def __init__(self, bot):
         Module.__init__(self, bot)
         self.links = []
+        self.cooldowns = {}
         self.json_handlers.append(JsonHandler(self, self.dynamic_path,
                                               attr='links', ext='lnk', yaml=True,
                                               callback_name='_link_json_callback'))
@@ -47,6 +70,8 @@ class link(Module):
         for url in url_matches.groups():
             response = ''
             for provider in self.links:
+                if provider not in self.cooldowns:
+                    self.cooldowns[provider] = CaseInsensitiveDict()
                 matches = re.match(self.links[provider]['match'], url)
                 if matches:
                     # response = '*** {}: '.format(self.links[provider]['display_name'])
@@ -56,6 +81,20 @@ class link(Module):
                     complete_dict.update(self.get_required_values(provider))
                     for key, value in matches.groupdict().items():
                         complete_dict[key] = escape(value)
+
+                    # check cooldown
+                    server_name = event['server'].name
+                    if event['from_to'].is_user:
+                        from_to = event['from_to'].host
+                    else:
+                        from_to = event['from_to'].name
+
+                    if server_name not in self.cooldowns[provider]:
+                        self.cooldowns[provider][server_name] = event['server'].idict()
+                    if from_to not in self.cooldowns[provider][server_name]:
+                        self.cooldowns[provider][server_name][from_to] = Cooldown()
+                    if not self.cooldowns[provider][server_name][from_to].can_do():
+                        continue
 
                     # getting the actual file itself
                     api_url = self.links[provider]['url'].format(**complete_dict)
